e90aa8d239c8307d0aee4499fb0cd61da8c9e1b5
agentx-cgn <agentx.cgn@gmail.com>
2021-08-17 20:06:57 +0200

    safety

--- src/components/streamer/streamer.ts
+++ src/components/streamer/streamer.ts
@@ -4,13 +4,51 @@ import './streamer.scss';
 
 import Factory     from '../factory';
 
-import * as SimplePeer from 'simple-peer';
+import SimplePeer from 'simple-peer';
+// import * as SimplePeer from 'simple-peer';
+// import * as Peer from 'simple-peer';
 
 
 const Streamer = Factory.create('Streamer', {
 
   reset () {
-    this.peer = {} as  SimplePeer.Instance;
+    this.peer = {} as SimplePeer.Instance;
+
+    // get video/voice stream
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(gotMedia).catch(() => {})
+
+    function gotMedia (stream: any) {
+      var peer1 = new SimplePeer({ initiator: true, stream: stream })
+      var peer2 = new SimplePeer()
+
+      peer1.on('signal', data => {
+        peer2.signal(data)
+      })
+
+      peer2.on('signal', data => {
+        peer1.signal(data)
+      })
+
+      peer2.on('stream', stream => {
+        // got remote video stream, now let's show it in a video tag
+        var video = document.querySelector('#video1') as HTMLVideoElement;
+
+        if ('srcObject' in video) {
+          video.srcObject = stream
+        } else {
+          video.src = window.URL.createObjectURL(stream) // for older browsers
+        }
+
+        video.play();
+
+      })
+    }
+
+
+
   },
 
   view( vnode: any ) {
@@ -18,7 +56,7 @@ const Streamer = Factory.create('Streamer', {
     const width = vnode.attribs.width;
 
     return m('div.streamer.w-100',
-        m('div'),
+        m('video#video1', {width: 320, height: 240}),
     );
   },
 
